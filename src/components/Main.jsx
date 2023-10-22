3b2d007eda63cb69e267d2373c9b2da434cf560a
Zo Nirina <tomzonirina@gmail.com>
2023-10-22 18:19:45 +0300

    feat: finish of the responsive education

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -39,26 +39,34 @@ return (
                 <h1 className='font-epilogue heading-educ'>Educations</h1>
             </article>
             <article className='border border-yellow-400 grid nice w-full h-[60dvh]'>
-                    <div className='flex border border-red-400 max-sm:flex-col max-sm:justify-around sm:max-md:justify-around sm:max-md:flex-col'>
-                    <article className='border border-gray-950'>
-                        <h2 className='font-epilogue text-yellow-600 text-justify heading-two-education'>ESSGAM - Ecole Saint Gabriel Mahajanga</h2>
-                        <p className='font-epilogue text-justify para-one-education'>Diplôme de licence. Licence en Finance comptabilité</p>
+                    <div className='flex max-sm:flex-col max-sm:justify-around sm:max-md:justify-around sm:max-md:flex-col'>
+                    <article className='md:max-lg:w-[80%] lg:max-xl:w-[80%] xl:w-[80%] xl:flex xl:items-center lg:max-xl:flex lg:max-xl:items-center md:max-lg:flex md:max-lg:items-center'>
+                        <div className='child-article'>
+                            <h2 className='font-epilogue text-yellow-600 text-justify heading-two-education'>ESSGAM - Ecole Saint Gabriel Mahajanga</h2>
+                            <p className='font-epilogue text-justify para-one-education'>Diplôme de licence. Licence en Finance comptabilité</p>
+                        </div>
                     </article>
-                    <div className='border max-sm:flex max-sm:justify-center max-sm:items-center sm:max-md:flex sm:max-md:justify-center sm:max-md:items-center border-gray-950'>
-                        <button className='p-2.5 w-fit rounded-[20px] bg-amber-500 font-esteban text-white'>
-                            2021 - 2023
-                        </button>
+                    <div className='max-sm:flex max-sm:justify-center max-sm:items-center sm:max-md:flex sm:max-md:justify-center sm:max-md:items-center md:max-lg:w-[20%] xl:w-[20%] xl:flex xl:items-center lg:max-xl:w-[20%] lg:max-xl:flex lg:max-xl:items-center md:max-lg:flex md:max-lg:items-center'>
+                        <div className='btn-article xl:flex xl:justify-center lg:max-xl:flex lg:max-xl:justify-center md:max-lg:flex md:max-lg:justify-center'>
+                            <button className='p-2.5 w-fit rounded-[20px] bg-amber-500 font-esteban text-white'>
+                                2021 - 2023
+                            </button>
+                        </div>
                     </div>
                     </div>
                     <div className='flex max-sm:flex-col max-sm:justify-around sm:max-md:justify-around sm:max-md:flex-col'>
-                    <article className='border border-gray-950'>
-                        <h2 className='font-epilogue text-yellow-600 text-justify heading-two-education'>SAYNA</h2>
-                        <p className='font-epilogue text-justify para-one-education'>Spécialisation en Design UI/UX</p>
+                    <article className='md:max-lg:w-[80%] lg:max-xl:w-[80%] xl:w-[80%] xl:flex xl:items-center lg:max-xl:flex lg:max-xl:items-center md:max-lg:flex md:max-lg:items-center'>
+                        <div className='child-article'>
+                            <h2 className='font-epilogue text-yellow-600 text-justify heading-two-education'>SAYNA</h2>
+                            <p className='font-epilogue text-justify para-one-education'>Spécialisation en Design UI/UX</p>
+                        </div>
                     </article>
-                    <div className='border max-sm:flex max-sm:justify-center max-sm:items-center sm:max-md:flex sm:max-md:justify-center sm:max-md:items-center border-gray-950'>
-                        <button className='p-2.5 w-fit rounded-[20px] bg-amber-500 font-esteban text-white'>
-                            Mars 2023
-                        </button>
+                    <div className='max-sm:flex max-sm:justify-center md:max-lg:w-[20%] lg:max-xl:w-[20%] xl:w-[20%] xl:flex xl:items-center md:max-lg:flex md:max-lg:items-center lg:max-xl:flex lg:max-xl:items-center max-sm:items-center sm:max-md:flex sm:max-md:justify-center sm:max-md:items-center'>
+                        <div className='btn-article xl:flex xl:justify-center lg:max-xl:flex lg:max-xl:justify-center md:max-lg:flex md:max-lg:justify-center'>
+                            <button className='p-2.5 w-fit rounded-[20px] bg-amber-500 font-esteban text-white'>
+                                Mars 2023
+                            </button>
+                        </div>
                     </div>
                     </div>
             </article>
